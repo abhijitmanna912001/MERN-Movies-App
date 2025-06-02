cf7791cc3e622620f7d46b9c7187c6425cbc72c7
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-06-02 14:01:39 +0530

    Image Upload functionality implemented

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
 import genreRoutes from "./routes/genreRoutes.js";
-import moviesRoutes from './routes/moviesRoutes.js'
+import moviesRoutes from "./routes/moviesRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -20,5 +22,9 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/genre", genreRoutes);
 app.use("/api/v1/movies", moviesRoutes);
+app.use("/api/v1/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
